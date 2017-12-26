c68fd06890107c9e8db618bfc3a545b29b4003ab
Naim Jeem <naim36208@gmail.com>
2017-12-26 13:17:45 +0600

    Object-creation-pattern: Factory pattern

--- src/object-creation-pattern.js
+++ src/object-creation-pattern.js
@@ -6,12 +6,32 @@ let peopleConstructor = function(name, age, location) {
   this.location = location;
 
   this.printPerson = function() {
+    console.log('--- Constructor pattern ---')
     console.log(this.name + this.age + this.location);
   }
 };
 
-let person1 = new peopleConstructor('John ', '20 ', 'CA');
-let person2 = new peopleConstructor('Doe ', '21 ', 'SA');
+let constructorPerson = new peopleConstructor('John ', '20 ', 'CA');
 
-person1.printPerson();
-person2.printPerson();
+constructorPerson.printPerson();
+
+// Factory pattern
+
+let peopleFactory = function(name, age, location) {
+  let temp = {}
+
+  temp.name = name;
+  temp.age = age;
+  temp.location = location;
+
+  temp.printPerson = function() {
+    console.log('--- Factory pattern ---')
+    console.log(temp.name + temp.age + temp.location);
+  };
+
+  return temp;
+};
+
+let factoryPerson = peopleFactory('John ', '20 ', 'CA');
+
+factoryPerson.printPerson();
